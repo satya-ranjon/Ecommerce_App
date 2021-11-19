ac7a8b2fc3f981f787d289785a6fa4a95c0c386d
khokon <k15981598k@gmail.com>
2021-11-19 16:32:18 +0600

    search

--- Product_App/views.py
+++ Product_App/views.py
@@ -7,13 +7,19 @@ from .models import Products
 # Create your views here.
 
 def Shop(request):
-    return render(request,'Product_App/shop.html')
+    Product_list = Products.objects.all()
+    if request.method == 'GET':
+        search = request.GET.get('search','')
+        result = Products.objects.filter(name__icontains = search)
+        print(result)
+    print(Product_list)
+    return render(request,'Product_App/shop.html',{'Product_list':Product_list,'result':result,'search':search})
 
 
-class Home(ListView):
-    model = Products
-    context_object_name = 'Product_list'
-    template_name='Product_App/shop.html'
+# class Home(ListView):
+#     model = Products
+#     context_object_name = 'Product_list'
+#     template_name='Product_App/shop.html'
     
 class ProductDitels(DetailView,LoginRequiredMixin):
     model = Products
